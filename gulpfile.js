ef3eefa861c8255239316185e4259cba0aa5e142
Wee <fi3ework@gmail.com>
2018-01-21 16:24:30 +0800

    chore(gulpfile.js): add dev and build cli

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,39 @@
 const gulp = require('gulp'),
+    babel = require('gulp-babel'),
+    webpack = require('gulp-webpack'),
+    uglify = require('gulp-uglify'),
     sass = require('gulp-sass'),
+    del = require('del'),
     browserSync = require('browser-sync').create();
 
 /* ========== develop ========== */
+
+// babel
+gulp.task('babel-js', function () {
+    return gulp.src('./src/scripts/**/*.js')
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(gulp.dest('./source/scripts/'));
+});
+
+// webpack
+gulp.task('webpack', ['babel-js'], function () {
+    return gulp.src('')
+        .pipe(webpack({
+            entry: {
+                'main': './source/scripts/tag.js',
+            },
+            output: {
+                filename: '[name].js',
+            }
+        }))
+        .pipe(gulp.dest('./source/scripts/'));
+});
+
 // sass
 gulp.task('sass', function () {
-    return gulp.src('./source/scss/theme.scss')
+    return gulp.src('./src/scss/theme.scss')
         .pipe(sass({
             outputStyle: 'compressed'
         }).on('error', sass.logError))
@@ -13,6 +41,10 @@ gulp.task('sass', function () {
 });
 
 // browser-sync
+gulp.task('reload-js', ['webpack'], function (done) {
+    browserSync.reload();
+    done();
+});
 gulp.task('reload-css', ['sass'], function (done) {
     browserSync.reload();
     done();
@@ -23,15 +55,30 @@ gulp.task('reload-layout', function (done) {
     done();
 });
 
-gulp.task('dev', ['sass'], function () {
+gulp.task('dev', ['webpack', 'sass'], function () {
     browserSync.init({
         proxy: 'localhost:4000'
     });
-    gulp.watch(['./source/scss/**/*.scss'], ['reload-css']);
-    gulp.watch(['./layout/**/*.ejs'], ['reload-layout']);
+    gulp.watch(['./src/scripts/**/*.js'], ['reload-js']);
+    gulp.watch(['./src/scss/**/*.scss'], ['reload-css']);
+    gulp.watch(['./layout/**/*.pug'], ['reload-layout']);
 });
 
 
 /* ========== bulid ========== */
 
-gulp.task('build', ['sass']);
+// delete redundant js files 
+gulp.task('del-js', ['webpack'], function (cb) {
+    return del([
+        './source/scripts/**/*',
+        '!./source/scripts/main.js'
+    ], cb);
+});
+
+// uglify
+gulp.task('uglify-js', ['webpack'], function () {
+    return gulp.src('./source/scripts/main.js').pipe(uglify())
+        .pipe(gulp.dest('./source/scripts/'));
+});
+
+gulp.task('build', ['sass', 'del-js', 'uglify-js']);
